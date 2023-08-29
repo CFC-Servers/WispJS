38edae7049e0bf5c2d4d0a476517c671231c3ca8
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-08-29 15:24:09 -0700

    tsc it first?

---
+++ dist/wisp_api.js
@@ -0,0 +1,129 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WispAPI = void 0;
+const axios = require("axios");
+class WispAPI {
+    constructor(domain, uuid, token, logger) {
+        this.domain = domain;
+        this.uuid = uuid;
+        this.token = token;
+        this.logger = logger;
+    }
+    makeURL(path) {
+        return `${this.domain}/api/client/servers/${this.uuid}/${path}`;
+    }
+    makeRequest(method, path, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const url = this.makeURL(path);
+            const headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/vnd.wisp.v1+json",
+                "Authorization": `Bearer ${this.token}`
+            };
+            const request = () => __awaiter(this, void 0, void 0, function* () {
+                let response;
+                const requestData = { headers: headers };
+                if (method == "GET") {
+                    if (data !== null) {
+                        requestData.params = data;
+                    }
+                    response = yield axios.get(url, requestData);
+                }
+                else if (method == "POST") {
+                    response = yield axios.post(url, data, requestData);
+                }
+                else if (method == "DELETE") {
+                    response = yield axios.delete(url, requestData);
+                }
+                else if (method == "PUT") {
+                    response = yield axios.put(url, data, requestData);
+                }
+                else {
+                    throw new Error(`Invalid method: ${method}`);
+                }
+                return response;
+            });
+            this.logger.info(`Sending ${method} request to ${url}`);
+            return yield request();
+        });
+    }
+    // Meta
+    sendCommand(command) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.makeRequest("POST", "command", { command: command });
+        });
+    }
+    getWebsocketDetails() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield this.makeRequest("GET", "websocket");
+            return response.data;
+        });
+    }
+    getServerDetails() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.makeRequest("GET", "");
+        });
+    }
+    getResources() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.makeRequest("GET", "resources");
+        });
+    }
+    powerRequest(action) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.makeRequest("POST", "power", { signal: action });
+        });
+    }
+    // Filesystem
+    // TODO: Handle pagination
+    getDirectoryContents(path) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield this.makeRequest("GET", "files/directory", { path: path });
+            return response.data;
+        });
+    }
+    createDirectory(path) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.makeRequest("POST", "files/directory", { path: path });
+        });
+    }
+    readFile(path) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield this.makeRequest("GET", "files/read", { path: path });
+            return response.data.content;
+        });
+    }
+    writeFile(path, content) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = { path: path, content: content };
+            return yield this.makeRequest("POST", "files/write", data);
+        });
+    }
+    deleteFiles(paths) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this.makeRequest("POST", "files/delete", { paths: paths });
+        });
+    }
+    renameFile(path, newPath) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const data = { path: path, to: newPath };
+            return yield this.makeRequest("PUT", "files/rename", data);
+        });
+    }
+    // FastDL
+    syncFastDL() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.makeRequest("POST", "fastdl");
+        });
+    }
+}
+exports.WispAPI = WispAPI;
---
+++ dist/wisp_socket.js
@@ -0,0 +1,183 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WispSocket = void 0;
+const socket_io_client_1 = require("socket.io-client");
+// TODO: Handle errors better
+// TODO: Allow for no ghToken
+// TODO: Don't require a logger
+class WispSocket {
+    constructor(logger, url, token, ghToken) {
+        this.logger = logger;
+        this.url = url;
+        this.token = token;
+        this.ghToken = ghToken;
+    }
+    connect() {
+        return new Promise((resolve, reject) => {
+            let connectedFirst = false;
+            this.manager = new socket_io_client_1.Manager(this.url, {
+                addTrailingSlash: false,
+                autoConnect: true,
+                reconnection: true,
+                reconnectionAttempts: 50,
+                reconnectionDelay: 250,
+                timeout: 5000,
+                extraHeaders: {
+                    'Authorization': `Bearer ${this.token}`
+                }
+            });
+            const socket = this.manager.socket("/");
+            socket.on("connect", () => {
+                this.logger.info("Connected to WebSocket");
+                socket.emit("auth", this.token);
+            });
+            socket.on("error", (reason) => {
+                this.logger.error(`WebSocket error: ${reason}`);
+            });
+            socket.on("connect_error", (error) => {
+                this.logger.error(`WebSocket Connect error: ${error}`);
+                reject();
+            });
+            socket.on("disconnect", (reason) => {
+                this.logger.error(`Disconnected from WebSocket: ${reason}`);
+                if (reason === "io server disconnect") {
+                    this.logger.error("Server closed connection - retrying");
+                }
+            });
+            socket.on("reconnect", (attempts) => {
+                this.logger.error(`WebSocket succesfully reconnected. Attempts: ${attempts}`);
+            });
+            socket.on("reconnect_error", (error) => {
+                this.logger.error(`WebSocket failed to reconnect: ${error}`);
+            });
+            socket.on("reconnect_failed", () => {
+                this.logger.error(`WebSocket failed to reconnect after max attempts`);
+            });
+            socket.on("auth_success", () => {
+                this.logger.info("Auth success");
+                if (!connectedFirst) {
+                    connectedFirst = true;
+                    resolve();
+                }
+            });
+            this.socket = socket;
+            setTimeout(() => {
+                if (!connectedFirst) {
+                    reject();
+                }
+            }, 5000);
+        });
+    }
+    filesearch(query) {
+        return new Promise((resolve, reject) => {
+            let done = false;
+            this.socket.once("filesearch-results", (data) => {
+                done = true;
+                resolve(data);
+            });
+            this.socket.emit("filesearch-start", query);
+            setTimeout(() => {
+                if (!done) {
+                    reject();
+                }
+            }, 5000);
+        });
+    }
+    gitPull(dir) {
+        return new Promise((resolve, reject) => {
+            let isPrivate = false;
+            const finished = (success, output) => {
+                this.socket.removeAllListeners("git-pull");
+                this.socket.removeAllListeners("git-error");
+                this.socket.removeAllListeners("git-success");
+                const result = {
+                    output: output,
+                    isPrivate: isPrivate
+                };
+                if (success) {
+                    resolve(result);
+                }
+                else {
+                    reject(output);
+                }
+            };
+            const sendRequest = (includeAuth = false) => {
+                const data = { dir: dir };
+                if (includeAuth) {
+                    isPrivate = true;
+                    data.authkey = this.ghToken;
+                }
+                this.socket.emit("git-pull", data);
+            };
+            this.socket.once("git-pull", (data) => {
+                this.logger.info(`Updating ${data}`);
+            });
+            this.socket.once("git-success", (commit) => {
+                this.logger.info(`Addon updated to ${commit}`);
+                finished(true, commit);
+            });
+            this.socket.on("git-error", (message) => {
+                if (message === "Remote authentication required but no callback set") {
+                    this.logger.info(`Remote authentication required, trying again with authkey: ${dir}`);
+                    sendRequest(true);
+                }
+                else {
+                    this.logger.error(`Error updating addon: ${message}`);
+                    finished(false, "");
+                }
+            });
+            sendRequest();
+        });
+    }
+    gitClone(url, dir, branch) {
+        return new Promise((resolve, reject) => {
+            let isPrivate = false;
+            const finished = (success) => {
+                this.socket.removeAllListeners("git-clone");
+                this.socket.removeAllListeners("git-error");
+                this.socket.removeAllListeners("git-success");
+                if (success) {
+                    const result = {
+                        isPrivate: isPrivate
+                    };
+                    resolve(result);
+                }
+                else {
+                    reject();
+                }
+            };
+            const sendRequest = (includeAuth = false) => {
+                const data = { dir: dir, url: url, branch: branch };
+                if (includeAuth) {
+                    isPrivate = true;
+                    data.authkey = this.ghToken;
+                }
+                this.socket.emit("git-clone", data);
+            };
+            this.socket.once("git-clone", (data) => {
+                this.logger.info(`Cloning ${data}`);
+            });
+            this.socket.once("git-success", () => {
+                this.logger.info("Project successfully cloned");
+                finished(true);
+            });
+            this.socket.on("git-error", (message) => {
+                if (message === "Remote authentication required but no callback set") {
+                    this.logger.info(`Remote authentication required, trying again with authkey: ${dir}`);
+                    sendRequest(true);
+                }
+                else {
+                    this.logger.info(`Error cloning repo: ${message}`);
+                    finished(false);
+                }
+            });
+            sendRequest();
+        });
+    }
+    addConsoleListener(callback) {
+        this.socket.on("console", (data) => {
+            callback(data.line);
+        });
+    }
+}
+exports.WispSocket = WispSocket;
