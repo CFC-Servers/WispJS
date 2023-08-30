6b89d99a9077a5394f0dba77494f4722f34e9141
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-08-29 17:16:49 -0700

    Change target

--- dist/wisp.js
+++ dist/wisp.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const winston = require("winston");
 import { WispAPI } from "./wisp_api";
 import { WispSocket } from "./wisp_socket";
@@ -18,12 +9,10 @@ export class WispInterface {
         });
         this.api = new WispAPI(domain, uuid, token, this.logger);
     }
-    connect(ghPAT) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const websocketInfo = yield this.api.getWebsocketDetails();
-            this.logger.info(`Connecting to websocket at ${websocketInfo.url} - ${websocketInfo.token}`);
-            this.socket = new WispSocket(this.logger, websocketInfo.url, websocketInfo.token, ghPAT);
-            yield this.socket.connect();
-        });
+    async connect(ghPAT) {
+        const websocketInfo = await this.api.getWebsocketDetails();
+        this.logger.info(`Connecting to websocket at ${websocketInfo.url} - ${websocketInfo.token}`);
+        this.socket = new WispSocket(this.logger, websocketInfo.url, websocketInfo.token, ghPAT);
+        await this.socket.connect();
     }
 }
--- dist/wisp_api.js
+++ dist/wisp_api.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import axios from "axios";
 export class WispAPI {
     constructor(domain, uuid, token, logger) {
@@ -18,108 +9,82 @@ export class WispAPI {
     makeURL(path) {
         return `${this.domain}/api/client/servers/${this.uuid}/${path}`;
     }
-    makeRequest(method, path, data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const url = this.makeURL(path);
-            const headers = {
-                "Content-Type": "application/json",
-                "Accept": "application/vnd.wisp.v1+json",
-                "Authorization": `Bearer ${this.token}`
-            };
-            const request = () => __awaiter(this, void 0, void 0, function* () {
-                let response;
-                const requestData = { headers: headers };
-                if (method == "GET") {
-                    if (data !== null) {
-                        requestData.params = data;
-                    }
-                    response = yield axios.get(url, requestData);
+    async makeRequest(method, path, data) {
+        const url = this.makeURL(path);
+        const headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/vnd.wisp.v1+json",
+            "Authorization": `Bearer ${this.token}`
+        };
+        const request = async () => {
+            let response;
+            const requestData = { headers: headers };
+            if (method == "GET") {
+                if (data !== null) {
+                    requestData.params = data;
                 }
-                else if (method == "POST") {
-                    response = yield axios.post(url, data, requestData);
-                }
-                else if (method == "DELETE") {
-                    response = yield axios.delete(url, requestData);
-                }
-                else if (method == "PUT") {
-                    response = yield axios.put(url, data, requestData);
-                }
-                else {
-                    throw new Error(`Invalid method: ${method}`);
-                }
-                return response;
-            });
-            this.logger.info(`Sending ${method} request to ${url}`);
-            return yield request();
-        });
+                response = await axios.get(url, requestData);
+            }
+            else if (method == "POST") {
+                response = await axios.post(url, data, requestData);
+            }
+            else if (method == "DELETE") {
+                response = await axios.delete(url, requestData);
+            }
+            else if (method == "PUT") {
+                response = await axios.put(url, data, requestData);
+            }
+            else {
+                throw new Error(`Invalid method: ${method}`);
+            }
+            return response;
+        };
+        this.logger.info(`Sending ${method} request to ${url}`);
+        return await request();
     }
     // Meta
-    sendCommand(command) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield this.makeRequest("POST", "command", { command: command });
-        });
+    async sendCommand(command) {
+        return await this.makeRequest("POST", "command", { command: command });
     }
-    getWebsocketDetails() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield this.makeRequest("GET", "websocket");
-            return response.data;
-        });
+    async getWebsocketDetails() {
+        const response = await this.makeRequest("GET", "websocket");
+        return response.data;
     }
-    getServerDetails() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield this.makeRequest("GET", "");
-        });
+    async getServerDetails() {
+        return await this.makeRequest("GET", "");
     }
-    getResources() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield this.makeRequest("GET", "resources");
-        });
+    async getResources() {
+        return await this.makeRequest("GET", "resources");
     }
-    powerRequest(action) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield this.makeRequest("POST", "power", { signal: action });
-        });
+    async powerRequest(action) {
+        return await this.makeRequest("POST", "power", { signal: action });
     }
     // Filesystem
     // TODO: Handle pagination
-    getDirectoryContents(path) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield this.makeRequest("GET", "files/directory", { path: path });
-            return response.data;
-        });
+    async getDirectoryContents(path) {
+        const response = await this.makeRequest("GET", "files/directory", { path: path });
+        return response.data;
     }
-    createDirectory(path) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield this.makeRequest("POST", "files/directory", { path: path });
-        });
+    async createDirectory(path) {
+        return await this.makeRequest("POST", "files/directory", { path: path });
     }
-    readFile(path) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield this.makeRequest("GET", "files/read", { path: path });
-            return response.data.content;
-        });
+    async readFile(path) {
+        const response = await this.makeRequest("GET", "files/read", { path: path });
+        return response.data.content;
     }
-    writeFile(path, content) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const data = { path: path, content: content };
-            return yield this.makeRequest("POST", "files/write", data);
-        });
+    async writeFile(path, content) {
+        const data = { path: path, content: content };
+        return await this.makeRequest("POST", "files/write", data);
     }
-    deleteFiles(paths) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield this.makeRequest("POST", "files/delete", { paths: paths });
-        });
+    async deleteFiles(paths) {
+        return await this.makeRequest("POST", "files/delete", { paths: paths });
     }
-    renameFile(path, newPath) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const data = { path: path, to: newPath };
-            return yield this.makeRequest("PUT", "files/rename", data);
-        });
+    async renameFile(path, newPath) {
+        const data = { path: path, to: newPath };
+        return await this.makeRequest("PUT", "files/rename", data);
     }
     // FastDL
-    syncFastDL() {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield this.makeRequest("POST", "fastdl");
-        });
+    async syncFastDL() {
+        await this.makeRequest("POST", "fastdl");
     }
 }
